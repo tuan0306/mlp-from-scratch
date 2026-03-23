d444ebee5619afdadbbdb6a62ac2364f0f975ce6
tuan0306 <nguyentuan22032006@gmail.com>
2026-03-23 19:01:30 +0700

    Bo sung Network Layer de danh gia val

--- core/network.py
+++ core/network.py
@@ -15,15 +15,27 @@ class Network:
         for layer in self.layers:
             output=layer.forward(output)
         return output
-    def fit(self,X_train,y_train,epochs,learning_rate,verbose=True):
-        history=[]
+    def fit(self,X_train,y_train,epochs,learning_rate,verbose=True,X_val=None,y_val=None):
+        train_history=[]
+        val_history=[]
         for epoch in range(epochs):
             output=self.predict(X_train)
-            err=self.loss(y_train,output)
-            history.append(err)
+            train_err=self.loss(y_train,output)
+            train_history.append(train_err)
+            
             error=self.loss_prime(y_train,output)
             for layer in reversed(self.layers):
                 error=layer.backward(error,learning_rate)
+                
+            if X_val is not None and y_val is not None:
+                val_output=self.predict(X_val)
+                val_err=self.loss(y_val,val_output)
+                val_history.append(val_err) 
+                
             if verbose and (epoch+1)%100==0:
-                print(f"Epoch {epoch+1:4d}/{epochs} | Loss: {err:.6f}")
-        return history
+                msg=f"Epoch {epoch+1:4d}/{epochs} | Train_Loss: {train_err:.6f}"
+                if X_val is not None and y_val is not None:
+                    msg+=f' | Val_Loss: {val_err:.6f}'
+                print(msg)
+                    
+        return train_history,val_history
